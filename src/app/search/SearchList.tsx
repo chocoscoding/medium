8e37c021f4f64485afc675629f1c85ffc4eb56d7
chocoscoding <chocoscoding@gmail.com>
2024-09-25 14:20:00 +0100

    search skeleton

--- src/app/search/SearchList.tsx
+++ src/app/search/SearchList.tsx
@@ -1,38 +1,38 @@
-'use client'
-import { getStoryByTag } from '@/actions/getStories'
-import StoryItem from '@/components/StoryItem'
-import { Story } from '@prisma/client'
-import { usePathname, useSearchParams } from 'next/navigation'
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+"use client";
+import { getStoryByTag } from "@/actions/getStories";
+import StoryItem from "@/components/StoryItem";
+import StoryItemSkeleton from "@/components/Skeletons/StoryItemSkeleton";
+import { Story } from "@prisma/client";
+import { usePathname, useSearchParams } from "next/navigation";
+import React, { useState, useEffect } from "react";
 
-type Props = {}
+type Props = {};
 
 const SearchList = (props: Props) => {
-    const [filteredStories, setFilteredStories] = useState<Story[]>([])
-    const searchparams = useSearchParams()
-    const searchValue = searchparams.get('for')
-    
-    useEffect(() => {
-        const fetchStory = async () => {
-            try {
-                const response = await getStoryByTag(searchValue || 'All')
-                setFilteredStories(response.stories)
-            } catch (error) {
-                console.log("Error in fetching teh data")
-            }
-        }
+  const [filteredStories, setFilteredStories] = useState<Story[]>([]);
+  const [loading, setLoading] = useState(true);
+  const searchparams = useSearchParams();
+  const searchValue = searchparams.get("for");
 
-        fetchStory()
-    },[searchparams])
+  useEffect(() => {
+    const fetchStory = async () => {
+      setLoading(true);
+      try {
+        const response = await getStoryByTag(searchValue || "All");
+        setFilteredStories(response.stories);
+      } catch (error) {
+        console.log("Error in fetching the data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStory();
+  }, [searchparams]);
+
   return (
-    <div>
-        {filteredStories.map((story) => (
-            <StoryItem key={story.id} story={story} />
-        ))}
-    </div>
-  )
-}
+    <div>{loading ? <StoryItemSkeleton amount={5} /> : filteredStories.map((story) => <StoryItem key={story.id} story={story} />)}</div>
+  );
+};
 
-export default SearchList
+export default SearchList;
